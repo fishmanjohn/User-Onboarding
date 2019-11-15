8a8a480cbacc97a003d59930619eb01a5aa2a9c0
fishmanjohn <john@emmaandjohnbyvan.com>
2019-11-14 22:48:24 -0800

    mvp fool

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LogForm from './components/form'
+import FormikLogForm from './components/form'
 
 function App() {
   return (
     <div className="App">
        <h1>Log in!</h1>
         <h2>It doen't matter to what just do it!</h2>
-      <LogForm/>
+      <FormikLogForm/>
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,32 +1,98 @@
-import React,{useState}from 'react';
+import React,{useState, useEffect}from 'react';
 import { withFormik, Form, Field} from 'formik';    
+import * as Yup from 'yup';
+import axios from 'axios';
 
+function LogForm ({values, touched, errors,status}){
 
-function normForm (){
+    const [users, setUsers]= useState([]);
+
+    useEffect(()=>{
+        status && setUsers(users => [...users,status])
+    },[status])
 
 return(
     <div>
        
         <Form>
-        <Field as='text'name='name' placeholder='Name'/>
+        <Field 
+        type='text'
+        name='name' 
+        placeholder='Name'
+          
+          />
+          {touched.name&&errors.name&&(<p>{errors.name}</p>)}
+
         <br/>
-        <Feild as='email' name='email' placeholder = 'email'/>
+
+        <Field
+         type='email'
+         name='email'
+          placeholder = 'Email'  
+          />
+        {touched.email&&errors.email&&(<p>{errors.email}</p>)}
         <br/>
-        <Feild as ='password'name='password'placeholder ='password'/>
+
+        <Field 
+        type ='password'
+        name='password'
+        placeholder ='password'
+        />
+        {touched.password&&errors.password&&(<p>{errors.password}</p>)}
         <br/>
+
         <label>
-        <Feild as ='checkbox' name='terms and conditons' placeholder= 'terms and conditons'/>
+
+        <Field
+         type ='checkbox'
+         name='terms' 
+         placeholder= 'Terms and Conditons' 
+         checked = {values.terms}
+         />
+
         Accept tems and conditons.</label>
+        {touched.terms&&errors.terms&&(<p>{errors.terms}</p>)}
         <br/>
-        <button>Submit</button>
+
+        <button type='submit'>Submit</button>
         </Form>
-    
+        {users.map(user =>(
+            <div>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        </div>
+        ))}
        
     </div>
+   
+    
 )
-
-
 }
 
-const LogForm = withFormik(normForm)
-export default LogForm;
+
+const FormikLogForm = withFormik({
+    mapPropsToValues({name,email,password,terms}){
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            terms: terms|| false,
+
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required().min(6,'password is too short - must be 6 characters minimum.')
+    }),
+
+    handleSubmit(values, {setStatus}){
+        axios
+        .post('https://reqres.in/api/users', values)
+        .then(res => {setStatus(res.data);})
+        .catch(err => console.log(err.response))
+    }
+})(LogForm)
+
+export default FormikLogForm;
+console.log(FormikLogForm)
